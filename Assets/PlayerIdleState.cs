946c666edaf8da90d079ebb1fe4267be130ab720
gjcdanijel <gajicdanijel5@gmail.com>
2024-04-19 14:46:00 +0200

    Player movement states and platform.

---
+++ Assets/PlayerIdleState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerIdleState : PlayerGroundedState
+{
+    public PlayerIdleState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName):base(_player,_stateMachine, _animBoolName){
+
+    }
+    public override void Enter(){
+        base.Enter();
+    }
+    public override void Exit(){
+        base.Exit();
+    }
+    public override void Update(){
+        base.Update();
+
+        if(xInput != 0)
+            stateMachine.ChangeState(player.moveState);
+        // if(Input.GetKeyDown(KeyCode.N))
+        // stateMachine.ChangeState(player.idleState);
+    }
+    
+}
